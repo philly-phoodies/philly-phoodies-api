2769afd344198685382434c7f389add7161d816d
Joseph Mulray <joe.mulray@gmail.com>
2019-01-29 15:22:58 -0500

    adding development config file

---
+++ config.py
@@ -0,0 +1,7 @@
+class DevelopmentConfig():
+    DEBUG = True
+    HOST = "0.0.0.0"
+    PORT = 8080
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:myvyb@localhost/MyVyb'
+    SECRET_TOKEN = 'SECRET TOKEN'
+
